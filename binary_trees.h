cdc0af95d0ef490cb4bcf017c399fb7f08d2c8ef
ralexrivero <ralexrivero@gmail.com>
2021-12-02 23:23:07 -0300

    fix header prototypes for struct

--- binary_trees.h
+++ binary_trees.h
@@ -21,6 +21,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 /***************/
 /* Prototypes */

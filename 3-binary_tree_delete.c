d04ed40ca28765df9352fefb632ff0480fb18834
ralexrivero <ralexrivero@gmail.com>
2021-12-02 23:28:34 -0300

    delete

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -14,9 +14,5 @@ void binary_tree_delete(binary_tree_t *tree)
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
 
-	tree->parent = NULL;
-	tree->left = NULL;
-	tree->right = NULL;
-
 	free(tree);
 }

4872d2296f65fcbb363f2e0a7d1a131e08f6f94a
ralexrivero <ralexrivero@gmail.com>
2021-12-02 23:57:49 -0300

    fix betty style

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -4,7 +4,7 @@
  * binary_tree_leaves - counts the leaves in a binary tree
  *
  * @tree: If tree is NULL, the function must return 0
- * A NULL pointer is not a leaf
+ * Return: A NULL pointer is not a leaf
  */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)

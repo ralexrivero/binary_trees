c7ce0b86da0c4b09306f233d520eca8002d3a89c
ralexrivero <ralexrivero@gmail.com>
2021-12-04 00:06:55 -0300

    files

---
+++ 133-heap_extract.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * heap_to_sorted_array - converts a Binary Max Heap to a sorted array of integers
+ *
+ * @root: double pointer to the root node of heap
+ * Return: value stored in the root node
+ */
+int heap_extract(heap_t **root)
+{
+	return (root);
+}
---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * heap_to_sorted_array - converts a Binary Max Heap to a sorted array of integers
+ *
+ * @heap: pointer to the root node of the heap to convert
+ * @size: address to store the size of the array
+ * Return: array sorted
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+	return (heap);
+}
